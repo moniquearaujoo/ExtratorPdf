c2a4b70a11c66b72544cd7daa99ca302e6624749
Monique Araujo <moniquearjm@gmail.com>
2025-04-28 19:19:34 -0300

    adicionando o procfile com o gunicorn e corrigindo o run na main

--- main.py
+++ main.py
@@ -660,4 +660,4 @@ def adicionar_planilha():
     return jsonify(resultado)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    # app.run(debug=True, host='0.0.0.0', port=5000)
